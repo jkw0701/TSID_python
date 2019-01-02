4ebdcbd9f9fddb369adeb42ac6997ee139ef63d9
Keunwoo Jang <jkw0701@snu.ac.kr>
2019-01-02 21:46:37 +0900

    TSID_python version

---
+++ __init__.py
@@ -0,0 +1,9 @@
+
+#from Contacts import *
+from Formulations import *
+from Math import *
+from Robots import *
+from Solvers import *
+#from Tasks import *
+#import Trajectories
+#from Utils import *
---
+++ conf.py
@@ -0,0 +1,5 @@
+
+import numpy as np
+
+JOINT_ACC_LB = np.array(-10.0 * np.ones(6))
+JOINT_ACC_UB = np.array(10.0 * np.ones(6))
